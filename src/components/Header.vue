ae6c3adbaf43bb9d1f60fb2eea1543411b525473
Yin <yinwu0025@gmail.com>
2022-04-17 00:07:20 +0100

    refine the style of header butoon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,22 +9,22 @@ export default {
 <template>
   <header class="h-20 leading-80px text-center bg-white border-b border-b-gray-400 flex justify-between px-8 py-4">
     <div class="flex gap-8">
-      <span class="inline-block w-12 h-12 bg-primary-10 rounded-full cursor-pointer" />
+      <span class="btn-header bg-primary-10" />
       <button
-        class="flex inline-block w-12 h-12 p-2 bg-white rounded-full shadow-custom"
+        class="btn-header"
       >
-        <span class="i-custom:idea inline-block w-8 h-8 text-blacks-70" />
+        <span class="i-custom:idea icon-header" />
       </button>
     </div>
     <div v-if="isEdit" class="flex gap-8">
-      <button class="flex inline-block w-12 h-12 p-2 bg-white rounded-full shadow-custom">
-        <span class="i-custom:save inline-block w-8 h-8 text-blacks-70" />
+      <button class="btn-header">
+        <span class="i-custom:save icon-header" />
       </button>
-      <button class="flex inline-block w-12 h-12 p-2 bg-white rounded-full shadow-custom">
-        <span class="i-custom:load inline-block w-8 h-8 text-blacks-70" />
+      <button class="btn-header">
+        <span class="i-custom:load icon-header" />
       </button>
-      <button class="flex inline-block w-12 h-12 p-2 bg-primary-100 rounded-full shadow-custom">
-        <span class="i-custom:download inline-block w-8 h-8 text-white" />
+      <button class="btn-header bg-primary-100">
+        <span class="i-custom:download icon-header text-white" />
       </button>
     </div>
   </header>
--- unocss.config.ts
+++ unocss.config.ts
@@ -14,6 +14,8 @@ export default defineConfig({
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['btn-header', 'flex inline-block w-12 h-12 p-2 bg-white rounded-full shadow-custom'],
+    ['icon-header', 'inline-block w-8 h-8 text-blacks-70'],
   ],
   presets: [
     presetUno(),
