5d4367abd0da3474acea2c64f491b334256a6fc3
Ryan Hsieh <chengyu0827@gmail.com>
2022-04-17 23:10:29 +0100

    feat: remove ssg/ssr

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
-import type { ViteSSGContext } from 'vite-ssg'
+import type { App } from 'vue'
+import type { RouteRecordRaw, Router } from 'vue-router'
+import type { HeadClient } from '@vueuse/head'
 
-export type UserModule = (ctx: ViteSSGContext) => void
+interface AppContext<HasRouter extends boolean = true> {
+  app: App<Element>
+  router: HasRouter extends true ? Router : undefined
+  routes: HasRouter extends true ? RouteRecordRaw[] : undefined
+  head: HeadClient | undefined
+}
+
+export type UserModule = (ctx: AppContext) => void
