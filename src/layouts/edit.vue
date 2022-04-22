f6c7d93ffaa93ad57cfb1729f7fa21d6e738df35
Yin <yinwu0025@gmail.com>
2022-04-22 22:25:26 +0100

    draft version (layout)

--- src/layouts/edit.vue
+++ src/layouts/edit.vue
@@ -2,9 +2,9 @@
   <main h-full>
     <Header is-edit="true" />
     <div sm:flex sm:flex-row class="content">
-      <div class="text-center bg-white sm:flex-1 sm:border sm:border-t-0 border-blacks-20">
+      <div class="text-center bg-white sm:flex-1 sm:border sm:border-t-0 border-blacks-20 relative">
         <CVPreview />
-        <div class="fixed items-center bottom-1/4 z-1">
+        <div class="flex justify-center absolute bottom-20px left-0 right-0">
           <Toolbar />
         </div>
       </div>
