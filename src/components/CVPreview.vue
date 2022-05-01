74408c5451682e3f9d1821385b3bde2fecbe445b
Ryan Hsieh <chengyu0827@gmail.com>
2022-05-01 14:03:42 +0100

    updated cv preview

--- src/components/CVPreview.vue
+++ src/components/CVPreview.vue
@@ -1,5 +1,56 @@
 <template>
-  <div px-4 py-8>
-    CV Preview
+  <div class="px-45 py-16">
+    <div class="p-6 shadow-custom">
+      <div class="p-2 flex items-baseline gap-4">
+        <span class="font-normal text-primary-100 text-4xl leading-[41px]">PEI-SHAN YU</span>
+        <span class="font-normal text-blacks-100 text-sm leading-4">UI/UX designer</span>
+      </div>
+      <div class="flex">
+        <div class="flex flex-col gap-4 w-[75%]">
+          <section class="p-2 flex flex-col gap-2">
+            <div class="flex gap-4">
+              <span class="hashtag text-primary-100 bg-primary-10">#TeamPlayer</span>
+              <span class="hashtag text-primary-100 bg-primary-10">#SelfMotivated</span>
+              <span class="hashtag text-primary-100 bg-primary-10">#CanDoAttitude</span>
+            </div>
+            <p class="paragraph-default text-blacks-70">
+              As a 2yr experienced UI designer, I see myself as a self-motivated and passionate learner, who makes progress every single day. With a can-do attitude, I try my best to experience the whole process of user interface and experience design, from stakeholder and user interviews, UI mock-ups and specifications, to interactive prototypes. With my educational training from science background and visual storytelling, I believe I am a nice candidate with strong systemic thinking and enjoyable visual design ability, to be considered for your team.
+              <br>
+              <br>
+              With getting inspired from talking to different people from diverse cultures and gathering designs from daily life, my strength is to be a good team player and be able to connect dots and build the connection from what I have learned and experienced from various fields.
+            </p>
+          </section>
+          <section>
+            <div class="px-2 py-1 subtitle-default text-primary-100">
+              EXPERIENCE
+            </div>
+            <div class="p-2 flex flex-col gap-1">
+              <span class="title-default text-blacks-100">
+                UI Designer
+              </span>
+              <div class="flex justify-between">
+                <span class="subtitle-default text-blacks-40">
+                  New Socks Media
+                </span>
+                <span class="subtitle-default text-blacks-40">
+                  Feb 2021 - Current
+                </span>
+              </div>
+              <p class="paragraph-default text-blacks-70">
+                To work closely with CEO, UX designer, solution architect, and developer to build digital products, including website and mobile applications.
+                2+ year experience of user-centric design
+                Translated business requirements and customer insights into wireframes, UI mock-ups and specifications, and interactive prototypes
+                Conducted User experience research through stack holder and user interviews
+                Familiar with developing brand guidelines and component libraries
+                Built SaaS products and got involved from the very early stage
+              </p>
+            </div>
+          </section>
+        </div>
+        <div class="w-[25%]">
+          Skill
+        </div>
+      </div>
+    </div>
   </div>
 </template>
