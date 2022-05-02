fcadb6cdcdcdbf7f1b867d397b20e07451978382
Ryan Hsieh <chengyu0827@gmail.com>
2022-05-02 20:29:53 +0100

    modified redirect router

--- src/pages/template/index.vue
+++ src/pages/template/index.vue
@@ -61,7 +61,10 @@ const router = useRouter()
           {{ t('template.button.back') }}
         </span>
       </button>
-      <button class="w-[294px] h-12 bg-primary-100 rounded-xl text-white px-10 py-3" @click="router.push('edit')">
+      <button
+        class="w-[294px] h-12 bg-primary-100 rounded-xl text-white px-10 py-3"
+        @click="router.push('/edit/about')"
+      >
         <span i-custom:collapse w-6 h-6 />
         <span class="font-normal text-lg leading-[1.375rem] vertical-middle ml-2">
           {{ t('template.button.next_step') }}
