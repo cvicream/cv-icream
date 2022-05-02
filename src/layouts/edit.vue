ce4b2d569244bbca7e62e4e1e21ac6674ab4bf75
yutingboomlai <64820792+yutingboomlai@users.noreply.github.com>
2022-05-02 18:02:28 +0800

    Updated CV preview (#4)
    * updated cv preview
    
    * add skill columns
    
    Co-authored-by: Ryan Hsieh <chengyu0827@gmail.com>

--- src/layouts/edit.vue
+++ src/layouts/edit.vue
@@ -3,7 +3,7 @@
     <Header is-edit="true" />
     <div sm:flex sm:flex-row class="content">
       <Toolbar sm:w-24 />
-      <CVPreview text-center bg-white sm:flex-1 sm:border sm:border-t-0 border-gray-400 />
+      <CVPreview bg-white sm:flex-1 sm:border sm:border-t-0 border-gray-400 />
       <Sidebar sm:w-96 />
     </div>
     <router-view />
