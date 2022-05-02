47c673bf1c5e73bdec9b58dd9f3291e84847747e
Ryan Hsieh <chengyu0827@gmail.com>
2022-05-02 12:19:06 +0100

    updated toolbar button style

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="w-110 h-20 text-center flex justify-between px-4 py-4 rounded-xl shadow-custom">
     <div class="btn-group-toolbar w-22 h-12">
-      <button class="btn-toolbar i-custom:undo w-8 h-8">
-        <!-- <span class="hover:flex hover:inline-block hover:w-8 hover:h-8 hover:p-2 hover:rounded-full hover:shadow-custom hover:bg-primary-10" /> -->
-      </button>
-      <button class="btn-toolbar i-custom:redo w-8 h-8" />
+      <div class="btn-toolbar">
+        <button class="i-custom:undo w-8 h-8" />
+      </div>
+      <div class="btn-toolbar">
+        <button class="i-custom:redo w-8 h-8" />
+      </div>
     </div>
     <div class="btn-group-toolbar w-42 h-12">
-      <button class="btn-toolbar i-custom:template w-8 h-8" />
-      <button class="btn-toolbar w-8 h-8">
-        <span class="icon-colour" />
-      </button>
-      <button class="btn-toolbar i-custom:font-size w-8 h-8" />
-      <button class="btn-toolbar i-custom:font-family w-8 h-8" />
+      <div class="btn-toolbar">
+        <button class="i-custom:template w-8 h-8" />
+      </div>
+      <div class="btn-toolbar w-8 h-8">
+        <div class="icon-colour" />
+      </div>
+      <div class="btn-toolbar">
+        <button class="i-custom:font-size w-8 h-8" />
+      </div>
+      <div class="btn-toolbar">
+        <button class="i-custom:font-family w-8 h-8" />
+      </div>
     </div>
     <div class="btn-group-toolbar w-22 h-12">
-      <button class="btn-toolbar i-custom:note w-8 h-8" />
-      <button class="btn-toolbar i-custom:feedback w-8 h-8" />
+      <div class="btn-toolbar">
+        <button class="i-custom:note w-8 h-8" />
+      </div>
+      <div class="btn-toolbar">
+        <button class="i-custom:feedback w-8 h-8" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .icon-colour {
-  width: 24px;
-  height: 24px;
-  padding: 1px;
+  width: 19.2px;
+  height: 19.2px;
   background-color: rgb(241, 139, 107, 1);
   border-radius: 50%;
   box-shadow:
--- unocss.config.ts
+++ unocss.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
     ['btn-radio', 'w-6 h-6 appearance-none border border-blacks-70 rounded-full bg-clip-content p-[3px] m-0 checked:bg-primary-70'],
     ['btn-header', 'flex inline-block w-12 h-12 p-2 bg-white rounded-full shadow-custom'],
     ['icon-header', 'inline-block w-8 h-8 text-blacks-70'],
-    ['btn-toolbar', 'flex inline-block w-8 h-8 text-blacks-70'],
-    ['btn-group-toolbar', 'flex gap-2 bg-white rounded-[69px] shadow-custom px-2 py-2'],
+    ['btn-toolbar', 'flex justify-center items-center text-blacks-70 hover:bg-primary-10 hover:rounded-full'],
+    ['btn-group-toolbar', 'flex justify-center items-center gap-2 bg-white rounded-[69px] shadow-custom px-2 py-2'],
   ],
   presets: [
     presetUno(),
