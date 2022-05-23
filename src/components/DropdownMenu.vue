9ab26bb1bd03035a096c0987c3c3f8e5ab17c80d
Yin <yinwu0025@gmail.com>
2022-05-23 17:15:47 +0100

    WIP

--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -18,11 +18,14 @@ export default {
       toggle,
     }
   },
+  method: {
+
+  },
 }
 </script>
 
 <template>
-  <div class="relative">
+  <div class="">
     <div class="btn-toolbar">
       <button
         class="w-8 h-8"
@@ -30,14 +33,17 @@ export default {
         @click="toggle"
       />
     </div>
-    <div v-if="isOpen" class="w-[232px] h-[168px] bg-white shadow-custom rounded-[20px] p-4 absolute left-0 -top-[200px] bg-red-400">
+    <div v-if="isOpen" class="w-[232px] h-[168px] bg-white shadow-custom rounded-[20px] p-4 absolute left-0 bottom-[72px]">
       <div class="flex justify-between items-center">
         <div class="flex gap-2 items-center">
-          <span class="i-custom:template icon-32" />
+          <span
+            class="w-8 h-8"
+            :class="icon"
+          />
           <label class="leading text-blacks-100">{{ label }}</label>
         </div>
         <span
-          class="i-custom:cancel icon-24"
+          class="i-custom:cancel icon-24 cursor-pointer"
           @click="toggle"
         />
       </div>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -8,21 +8,34 @@
         <button class="i-custom:redo w-8 h-8" />
       </div>
     </div>
-    <div class="btn-group-toolbar w-42 h-12">
-      <DropdownMenu label="Layout" icon="i-custom:template">
-        <div class="btn-toolbar">
-          <button class="i-custom:template icon-48" />
+    <div class="btn-group-toolbar w-42 h-12 relative">
+      <DropdownMenu label="Layout" icon="i-custom:template text-blacks-70">
+        <div class="btn-toolbar w-12 h-12">
+          <button class="i-custom:template-right w-8 h-8" />
         </div>
-        <div class="btn-toolbar">
-          <button class="i-custom:template icon-48" />
+        <div class="btn-toolbar w-12 h-12">
+          <button class="i-custom:template-left w-8 h-8" />
+        </div>
+        <div class="btn-toolbar w-12 h-12">
+          <button class="i-custom:template-full w-8 h-8" />
         </div>
       </DropdownMenu>
-      <div class="btn-toolbar w-8 h-8">
-        <div class="icon-colour" />
-      </div>
-      <div class="btn-toolbar">
-        <button class="i-custom:font-size w-8 h-8" />
-      </div>
+      <DropdownMenu label="Colour" icon="icon-colour">
+        <div class="btn-toolbar w-12 h-12">
+          <button class="icon-colour icon-48" />
+        </div>
+        <div class="btn-toolbar w-12 h-12">
+          <button class="icon-colour-yellow icon-48" />
+        </div>
+      </DropdownMenu>
+      <DropdownMenu label="Font Size" icon="i-custom:font-size text-blacks-70">
+        <div class="btn-toolbar w-12 h-12">
+          <button class="i-custom:font-size w-6 h-6" />
+        </div>
+        <div class="btn-toolbar w-12 h-12">
+          <button class="i-custom:font-size  w-8 h-8" />
+        </div>
+      </DropdownMenu>
       <div class="btn-toolbar">
         <button class="i-custom:font-family w-8 h-8" />
       </div>
@@ -38,14 +51,35 @@
   </div>
 </template>
 
-<style scoped>
+<style>
+:root {
+  --default-color: #F18B6BFF;
+  --yellow-color:  #FAAA1EFF;
+  --green-color:   #507F39FF;
+  --blue-color:    #4BA1B4FF;
+  --indigo-color:  #005B84FF;
+  --purple-color:  #766E8EFF;
+  --black-color:   #222222FF;
+}
+
 .icon-colour {
   width: 19.2px;
   height: 19.2px;
-  background-color: rgb(241, 139, 107, 1);
+  background-color: var(--default-color);
   border-radius: 50%;
   box-shadow:
-    0 0 0 0.4px rgb(255, 255, 255, 1),
+    0 0 0 0.4px #FFFFFFFF,
     0 0 0 2.4px rgb(252, 232, 225, 1);
 }
+
+.icon-colour-yellow {
+  width: 19.2px;
+  height: 19.2px;
+  background-color: var(--yellow-color);
+  border-radius: 50%;
+  box-shadow:
+    0 0 0 0.4px #FFFFFFFF,
+    0 0 0 2.4px calc(var(--yellow-color)- var(#0x33))
+}
+
 </style>
