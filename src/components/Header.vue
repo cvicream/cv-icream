4f0afc4112fed93be19b86c6920f171fcfb81fc9
Ryan Hsieh <chengyu0827@gmail.com>
2022-05-30 22:50:03 +0100

    added import and export function

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,80 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useUserStore } from '~/stores/user'
+
+const user = useUserStore()
+const {
+  layout,
+  template,
+  about,
+  summary,
+  experience,
+  project,
+  skill,
+  education,
+  certificate,
+  contact,
+} = storeToRefs(user)
+
 const props = defineProps<{
   isEdit: boolean
 }>()
+
 const router = useRouter()
+
+function exportJsonFile() {
+  const jsonData = {
+    layout: layout.value,
+    template: template.value,
+    about: about.value,
+    summary: summary.value,
+    experience: experience.value,
+    project: project.value,
+    skill: skill.value,
+    education: education.value,
+    certificate: certificate.value,
+    contact: contact.value,
+  }
+  const dataStr = JSON.stringify(jsonData)
+  const dataUri = `data:application/json;charset=utf-8,${encodeURIComponent(dataStr)}`
+
+  const fileNames = ['CV']
+  if (about.value.name) fileNames.push(about.value.name)
+  if (about.value.jobTitle) fileNames.push(about.value.jobTitle)
+  const exportFileDefaultName = `${fileNames.join('_')}.cvicream`
+
+  const linkElement = document.createElement('a')
+  linkElement.setAttribute('href', dataUri)
+  linkElement.setAttribute('download', exportFileDefaultName)
+  linkElement.click()
+}
+
+async function importJsonFile() {
+  const jsonFile = await getJsonUpload()
+  const obj = JSON.parse(jsonFile)
+  Object.keys(obj).forEach((key) => {
+    user.$patch((state) => {
+      state[key] = obj[key]
+    })
+  })
+}
+
+function getJsonUpload() {
+  return new Promise((resolve) => {
+    const inputFileElement = document.createElement('input')
+    inputFileElement.setAttribute('type', 'file')
+    inputFileElement.setAttribute('multiple', 'false')
+    inputFileElement.setAttribute('accept', '.cvicream')
+
+    inputFileElement.addEventListener('change', (event) => {
+      const { files } = event.target
+      if (!files) return
+
+      resolve(files[0].text())
+    }, false)
+    inputFileElement.click()
+  })
+}
 
 </script>
 
@@ -17,10 +89,10 @@ const router = useRouter()
       </button>
     </div>
     <div v-if="isEdit" class="flex gap-8">
-      <button class="btn-header">
+      <button class="btn-header" @click="exportJsonFile">
         <span class="i-custom:save icon-header" />
       </button>
-      <button class="btn-header">
+      <button class="btn-header" @click="importJsonFile">
         <span class="i-custom:load icon-header" />
       </button>
       <button
