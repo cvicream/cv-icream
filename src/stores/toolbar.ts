d0f9fa70b73486df45f12055b6c0024201f7dbce
Ryan Hsieh <chengyu0827@gmail.com>
2022-05-31 22:16:23 +0100

    Add toolbar dropdown menus (#13)
    * WIP
    
    * draft version (layout)
    
    * draft version for toolbar
    
    * updated toolbar button style
    
    * updated landing pages (#6)
    
    * draft version-landing page
    
    * add English version
    
    * fix radio button and refine code
    
    * adjust button size/font for landing page
    
    Co-authored-by: Yin <71213611+Yin-Wu-025@users.noreply.github.com>
    Co-authored-by: Yin <yinwu0025@gmail.com>
    
    * updated sidebar (#7)
    
    * feat: added sidebar menus
    
    * WIP
    
    * updated sidebar
    
    * modified redirect router
    
    * Updated sidebar (#8)
    
    * feat: added sidebar menus
    
    * WIP
    
    * updated sidebar
    
    * modified redirect router
    
    * updated sidebar
    
    * created DropdownMenu component
    
    * WIP
    
    * WIP
    
    * WIP
    
    * modified dropdown menu click action
    
    * WIP
    
    * updated colour change function
    
    * Update colour/ fontsize/ font family database
    
    Co-authored-by: Yin <yinwu0025@gmail.com>
    Co-authored-by: Yin <71213611+Yin-Wu-025@users.noreply.github.com>

---
+++ src/stores/toolbar.ts
@@ -0,0 +1,123 @@
+import { defineStore } from 'pinia'
+
+export const useToolbarStore = defineStore('toolbar', {
+  state: () => ({
+    dropdownMenu: {
+      layout: false,
+      colour: false,
+      fontSize: false,
+      fontFamily: false,
+    },
+    colours: [
+      {
+        name: 'default',
+        primary: '#F18B6B',
+        secondary: '#FCE8E1',
+        isActive: true,
+      },
+      {
+        name: 'yellow',
+        primary: '#FAAA1E',
+        secondary: '#FFF7E8',
+        isActive: false,
+      },
+      {
+        name: 'green',
+        primary: '#507F39',
+        secondary: '#EDF2EB',
+        isActive: false,
+      },
+      {
+        name: 'blue',
+        primary: '#4BA1B4',
+        secondary: '#EDF6F7',
+        isActive: false,
+      },
+      {
+        name: 'indigo',
+        primary: '#005B84',
+        secondary: '#E5EFF3',
+        isActive: false,
+      },
+      {
+        name: 'purple',
+        primary: '#766E8E',
+        secondary: '#F1F0F4',
+        isActive: false,
+      },
+      {
+        name: 'black',
+        primary: '#222222',
+        secondary: '#E9E9E9',
+        isActive: false,
+      },
+    ],
+    fontSize: [
+      {
+        name: 'small',
+        isActive: false,
+      },
+      {
+        name: 'default',
+        isActive: true,
+      },
+    ],
+    fontFamily: [
+      {
+        name: 'arial',
+        label: 'Arial',
+        isActive: false,
+      },
+      {
+        name: 'georgia',
+        label: 'Georgia',
+        isActive: false,
+      },
+      {
+        name: 'gill-sans',
+        label: 'Gill Sans',
+        isActive: true,
+      },
+      {
+        name: 'helvetica',
+        label: 'Helvetica',
+        isActive: false,
+      },
+      {
+        name: 'times-new-roman',
+        label: 'Times New Roman',
+        isActive: false,
+      },
+      {
+        name: 'lato',
+        label: 'Lato',
+        isActive: false,
+      },
+    ],
+  }),
+  actions: {
+    toggle(value: string) {
+      Object.keys(this.dropdownMenu).forEach((key) => {
+        if (key === value)
+          this.dropdownMenu[key as keyof typeof this.dropdownMenu] = !this.dropdownMenu[key as keyof typeof this.dropdownMenu]
+        else
+          this.dropdownMenu[key as keyof typeof this.dropdownMenu] = false
+      })
+    },
+    changeColour(index: number) {
+      this.colours.forEach((colour, i) => {
+        colour.isActive = i === index
+      })
+    },
+    changeFontSize(sizeType: string) {
+      this.fontSize.forEach((fontSize) => {
+        fontSize.isActive = (fontSize.name === sizeType)
+      })
+    },
+    changeFontFamily(index: number) {
+      this.fontFamily.forEach((fontFamily, i) => {
+        fontFamily.isActive = i === index
+      })
+    },
+  },
+})
