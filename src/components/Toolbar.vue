f7ed420f60da45c7add4ad29a8174c9d96883997
Ryan Hsieh <chengyu0827@gmail.com>
2022-06-26 00:15:20 +0100

    Change toolbar layout on mobile (#40)

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -37,7 +37,7 @@ window.addEventListener('click', onClick, false)
 </script>
 
 <template>
-  <div class="w-full h-20 text-center bg-white flex justify-between gap-4 px-4 py-4 border-t-1 border-blacks-20 sm:w-auto sm:border-0 sm:rounded-xl sm:shadow-custom">
+  <div class="w-full h-[81px] text-center bg-white flex justify-center gap-4 px-4 py-4 border-t-1 border-blacks-20 sm:w-auto sm-h-20 sm:border-0 sm:rounded-xl sm:shadow-custom">
     <div class="btn-group-toolbar w-22 h-12">
       <div class="btn-toolbar">
         <button class="i-custom:undo w-8 h-8" />
