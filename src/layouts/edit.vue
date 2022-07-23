c4e0a69873b023201696665f34b84c40c23b5682
Ryan Hsieh <chengyu0827@gmail.com>
2022-07-23 13:23:56 +0100

    Bugfix: design bar collapse icon size (#65)

--- src/layouts/edit.vue
+++ src/layouts/edit.vue
@@ -131,9 +131,9 @@ function resize() {
             class="flex sm:inline-flex absolute bottom-0 transition group"
             :class="isDesignBarOpen ? 'center' : 'right'"
           >
-            <button class="hidden sm:inline-block absolute bottom-full right-0 invisible group-hover:visible" @click="onCollapse">
+            <button class="w-5 h-7 pb-2 m-0 hidden sm:inline-block absolute bottom-full right-0 invisible group-hover:visible" @click="onCollapse">
               <span
-                class="icon-24"
+                class="w-5 h-5"
                 :class="isDesignBarOpen ? 'i-origin:close' : 'i-origin:open'"
               />
             </button>
