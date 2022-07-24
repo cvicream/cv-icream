20210cd324ed016c62d1ce0297c1db7c789647bb
Ryan Hsieh <chengyu0827@gmail.com>
2022-07-24 01:04:23 +0100

    added feedback form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { storeToRefs } from 'pinia'
 import { useUserStore } from '~/stores/user'
 import { useToolbarStore } from '~/stores/toolbar'
-import { getJsonUpload, stripHtml } from '~/utils'
+import { getJsonUpload, stripHtml, validateEmail } from '~/utils'
 
 const props = defineProps<{
   isEdit: boolean
 }>()
 
 const isActionActive = ref(false)
+const feedbackModalVisible = ref(false)
+const content = ref('')
+const name = ref('')
+const email = ref('')
+const feedbackEnable = computed(() => {
+  return content.value && name.value && email.value && validateEmail(email.value)
+})
 
 const router = useRouter()
 
@@ -107,6 +114,27 @@ function closeAction() {
   isActionActive.value = false
 }
 
+function toggleFeedback() {
+  feedbackModalVisible.value = !feedbackModalVisible.value
+}
+
+function resetFeedback() {
+  name.value = ''
+  email.value = ''
+  content.value = ''
+}
+
+async function sendFeedback() {
+  const url = 'https://script.google.com/macros/s/AKfycbxq_a3TUrbsvWHxASqFqouHQ-12J2QQbYjvyumY4-DCc2d_Kb07pBAe_-NCuQ9t878Z/exec'
+  await fetch(`${url}?${new URLSearchParams({
+    name: name.value,
+    email: email.value,
+    content: content.value,
+  })}`)
+  feedbackModalVisible.value = false
+  resetFeedback()
+}
+
 </script>
 
 <template>
@@ -118,7 +146,7 @@ function closeAction() {
       <button class="btn-icon-32">
         <span class="i-custom:idea w-6 h-6" />
       </button>
-      <button class="btn-icon-32">
+      <button class="btn-icon-32" @click="toggleFeedback">
         <span class="i-custom:feedback w-6 h-6" />
       </button>
     </div>
@@ -150,4 +178,58 @@ function closeAction() {
     </div>
   </header>
   <div class="border-b border-b-blacks-20" />
+
+  <Modal
+    v-show="feedbackModalVisible"
+    @close="feedbackModalVisible = false"
+  >
+    <div class="px-2 pb-4">
+      <div class="leading text-primary-100 mt-1">
+        Face a Problem or Need Help?
+      </div>
+      <div class="paragraph text-blacks-70">
+        Leave us a message. We will get back to you as soon as possible : )
+      </div>
+      <textarea
+        v-model="content"
+        placeholder="Share your feedback here..."
+        class="form-textarea bg-primary-10 custom-scrollbar mt-8"
+      />
+      <div class="mt-6">
+        <label class="note text-blacks-70">Your Contact Info</label>
+        <input
+          v-model="name"
+          type="search"
+          placeholder="Name"
+          class="form-input bg-primary-10 mt-1"
+        >
+        <input
+          v-model="email"
+          type="search"
+          placeholder="Email"
+          class="form-input bg-primary-10 mt-3"
+        >
+      </div>
+      <div class="flex flex-col gap-6 mt-8 sm:flex-row sm:justify-between">
+        <button
+          class="btn-secondary px-8 flex-shrink-0"
+          @click="feedbackModalVisible = false"
+        >
+          <span class="subleading">
+            Next Time
+          </span>
+        </button>
+        <button
+          class="btn-primary px-8 flex-grow flex-shrink-0"
+          :class="{ 'text-blacks-40 bg-blacks-10': !feedbackEnable}"
+          :disabled="!feedbackEnable"
+          @click="sendFeedback"
+        >
+          <span class="subleading">
+            Send
+          </span>
+        </button>
+      </div>
+    </div>
+  </Modal>
 </template>
