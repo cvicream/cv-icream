1bc50be3137ba3a9b931799fc7b5964f421427e7
Yin <yinwu0025@gmail.com>
2022-09-19 21:00:21 +0100

    Popup window for deleting blocks

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     Counter: typeof import('./components/Counter.vue')['default']
     CVPreview: typeof import('./components/CVPreview.vue')['default']
+    DeleteBlockModal: typeof import('./components/DeleteBlockModal.vue')['default']
     DropdownMenu: typeof import('./components/DropdownMenu.vue')['default']
     Editor: typeof import('./components/Editor.vue')['default']
     FeedbackModal: typeof import('./components/FeedbackModal.vue')['default']
