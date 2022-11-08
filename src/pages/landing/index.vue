cb8fc91ae042e6945d232dbb0f4703c693d76329
yutingboomlai <64820792+yutingboomlai@users.noreply.github.com>
2022-11-08 22:26:49 +0800

    add a judgement of uploading file type (#99)
    * add a judgement of uploading file type
    
    * update reminder context and buttons' position
    
    * tidy up
    
    * fix merge bug in landing page
    
    Co-authored-by: yuting <yuting@webglsoft.com>
    Co-authored-by: yuting <yutginboom@gmail.com>
    Co-authored-by: Ryan Hsieh <chengyu0827@gmail.com>

--- src/pages/landing/index.vue
+++ src/pages/landing/index.vue
@@ -3,39 +3,43 @@ import { useUserStore } from '~/stores/user'
 
 import { useToolbarStore } from '~/stores/toolbar'
 import { getJsonUpload } from '~/utils'
+import { DRAFT_FILE_TYPE } from '~/constants'
 
 const user = useUserStore()
 const toolbar = useToolbarStore()
 const { t } = useI18n()
+const uploadFiletype = ref(false)
 const router = useRouter()
 
-async function upload() {
-  await importJsonFile()
-  redirectToEdit()
-}
-
 async function importJsonFile() {
-  const jsonFile = await getJsonUpload()
-  const obj = JSON.parse(jsonFile as string)
-  Object.keys(obj).forEach((key) => {
-    if (key === 'user') {
-      const subObj = obj[key]
-      Object.keys(subObj).forEach((subKey) => {
-        user.$patch((state) => {
-          state[subKey] = subObj[subKey]
+  uploadFiletype.value = false
+  try {
+    const json = await getJsonUpload()
+    const obj = JSON.parse(json as string)
+    Object.keys(obj).forEach((key) => {
+      if (key === 'user') {
+        const subObj = obj[key]
+        Object.keys(subObj).forEach((subKey) => {
+          user.$patch((state) => {
+            state[subKey] = subObj[subKey]
+          })
         })
-      })
-      user.updateTimestamp()
-    }
-    else if (key === 'toolbar') {
-      const subObj = obj[key]
-      Object.keys(subObj).forEach((subKey) => {
-        toolbar.$patch((state) => {
-          state.currentState[subKey] = subObj[subKey]
+        user.updateTimestamp()
+      }
+      else if (key === 'toolbar') {
+        const subObj = obj[key]
+        Object.keys(subObj).forEach((subKey) => {
+          toolbar.$patch((state) => {
+            state.currentState[subKey] = subObj[subKey]
+          })
         })
-      })
-    }
-  })
+      }
+    })
+    redirectToEdit()
+  }
+  catch (error) {
+    uploadFiletype.value = true
+  }
 }
 
 function redirectToEdit() {
@@ -70,7 +74,7 @@ const onNext = () => {
       </button>
       <button
         class="p-6 flex justify-center items-center w-81 h-63 sm:w-101 sm:h-66 rounded-xl flex-col gap-5 border-primary-100 shadow-custom hover:border-1 hover:bg-primary-10"
-        @click="upload"
+        @click="importJsonFile"
       >
         <div class="w-12 h-12 flex justify-center items-center p-2 rounded-full border-1 border-transparent shadow-custom">
           <span class="i-custom:load w-8 h-8" />
@@ -84,6 +88,32 @@ const onNext = () => {
       </button>
     </div>
   </div>
+
+  <Modal
+    v-show="uploadFiletype"
+    title="Upload Your CV Draft"
+    :subtitle="`Please upload the file with the format in ‘.${DRAFT_FILE_TYPE}’.`"
+    @close="uploadFiletype = false"
+  >
+    <div class="flex justify-between gap-6 mt-6 sm:flex-row">
+      <button
+        class="btn-secondary px-12 flex-shrink-0"
+        @click="uploadFiletype = false"
+      >
+        <span class="subleading">
+          Cancel
+        </span>
+      </button>
+      <button
+        class="btn-primary px-12 flex-shrink-0"
+        @click="importJsonFile"
+      >
+        <span class="subleading">
+          Got it!
+        </span>
+      </button>
+    </div>
+  </Modal>
 </template>
 
 <route lang="yaml">
