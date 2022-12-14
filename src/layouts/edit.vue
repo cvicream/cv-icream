a10a52f87b248b9c72b94c692a8c4235febef5bf
Ryan Hsieh <chengyu0827@gmail.com>
2022-12-14 21:38:54 +0000

    Bugfix: zoom fit (#131)
    * fix zoom fit
    
    * tidy up

--- src/layouts/edit.vue
+++ src/layouts/edit.vue
@@ -5,7 +5,7 @@ import { storeToRefs } from 'pinia'
 import { useUserStore } from '~/stores/user'
 import { useToolbarStore } from '~/stores/toolbar'
 import { getColor, getStorage, hasStorage, isMobileDevice, setCssVariable, setStatus } from '~/utils'
-import { A4_HEIGHT_PX, MAX_SIDEBAR_WIDTH, MIN_SIDEBAR_WIDTH, MOBILE_BREAKPOINT, PAGE_BREAKPOINT, SCALES } from '~/constants'
+import { A4_HEIGHT_PX, A4_WIDTH_PX, MAX_SIDEBAR_WIDTH, MIN_SIDEBAR_WIDTH, MOBILE_BREAKPOINT, PAGE_BREAKPOINT, SCALES } from '~/constants'
 
 const user = useUserStore()
 const toolbar = useToolbarStore()
@@ -215,7 +215,7 @@ function zoomOut() {
 function zoomFit() {
   if (leftSide.value) {
     const { width, height } = getElementInnerDimensions(leftSide.value)
-    scale.value = (width > height) ? Math.floor(height * 100 / scaleA4Height.value) : Math.floor(width * 100 / 793.7)
+    scale.value = (width > height) ? Math.floor(height * 100 / A4_HEIGHT_PX) : Math.floor(width * 100 / A4_WIDTH_PX)
   }
 }
 
@@ -245,7 +245,7 @@ function handleRightPageClick() {
     <div class="w-full h-[calc(100%-137px)] border-b-1 border-blacks-20 sm:flex sm:flex-row sm:h-[calc(100%-57px)] sm:border-0 overflow-hidden">
       <div
         ref="leftSide"
-        class="h-[calc(100%-8px)] bg-white px-3 pt-7 overflow-auto custom-scrollbar flex-grow flex-shrink sm:px-7 sm:pt-15 m-1"
+        class="h-[calc(100%-8px)] bg-white px-3 py-7 overflow-auto custom-scrollbar flex-grow flex-shrink sm:px-7 sm:py-15 m-1"
         :class="{ 'absolute hidden': isMobile && !isCVPreviewVisible }"
       >
         <div
@@ -257,7 +257,7 @@ function handleRightPageClick() {
         >
           <div
             ref="cvPreview"
-            class="w-[210mm] min-h-[297mm] pb-7 sm:pb-15"
+            class="w-[210mm] min-h-[297mm]"
             :style="{
               transform: `scale(${scale}%)`,
               'transform-origin': '0 0',
