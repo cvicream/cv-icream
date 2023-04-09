e679c7b4d47c1b56a6a03646c1840545a2bbf12c
Yin <yinwu0025@gmail.com>
2023-04-09 12:48:25 +0800

    Reset redo storage for new operation

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -3,6 +3,7 @@ import _ from 'lodash'
 import type { UserModule } from '~/types'
 import { getStorage, hasStorage, isEditing, setStorage } from '~/utils'
 import { useUndoStore } from '~/stores/undo'
+import { useRedoStore } from '~/stores/redo'
 
 // Setup Pinia
 // https://pinia.esm.dev/
@@ -19,12 +20,22 @@ export const install: UserModule = ({ app }) => {
       if (isEditing()) {
         // exclude `undo` and `redo`
         const savedState = _.cloneDeep(_.omit(state, ['undo', 'redo']))
+        const undoStore = useUndoStore()
+        const redoStore = useRedoStore()
         setStorage(savedState)
 
-        if (['undo', 'redo'].includes(state.user.action)) { state.user.action = '' }
+        if (['undo', 'redo'].includes(state.user.action)) {
+          state.user.action = ''
+          if (redoStore.list.length === 1 && redoStore.maxRestore === 0)
+            redoStore.updateMaxRestore(undoStore.list.length + redoStore.list.length)
+        }
         else {
-          const undoStore = useUndoStore()
           undoStore.push(savedState)
+          if (redoStore.maxRestore !== 0
+              && (redoStore.list.length + undoStore.list.length) !== redoStore.maxRestore) {
+            redoStore.reset()
+            redoStore.updateMaxRestore(0)
+          }
         }
       }
     },
