997fbc9911ea7b75412b11113cfadeeb934d913f
Zack Chu <zack.chu@validusrm.com>
2023-05-07 22:14:53 +0100

    change background color

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -51,7 +51,7 @@ function toggleShowAll() {
       >
       <div
         v-else
-        class="hover:bg-blacks-20 hover:cursor-text w-full h-full flex items-center leading leading-6 text-blacks-100 text-ellipsis whitespace-nowrap overflow-hidden bg-transparent"
+        class="hover:bg-blacks-10 hover:cursor-text w-full h-full flex items-center leading leading-6 text-blacks-100 text-ellipsis whitespace-nowrap overflow-hidden bg-transparent"
         :title="item.name"
         @click="onChangToEdit"
       >
