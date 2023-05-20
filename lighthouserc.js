09190f164114adf21d27fb8dfa9e5396581967f4
Ryan Hsieh <chengyu0827@gmail.com>
2023-05-20 16:13:28 +0100

    Netlify plugin lighthouse (#155)
    * add netlify plugin lighthouse
    
    * update netlify.toml
    
    * update netlify.toml
    
    * add ci.yml
    
    * update ci.yml
    
    * update ci.yml
    
    * specify pnpm version in ci.yml
    
    * add lighthouse config
    
    * update assertions
    
    * update lighthouse config
    
    * update lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'color-contrast': 'off',
+        'csp-xss': 'off',
+        'font-display': 'off',
+        'meta-viewport': 'off',
+        'service-worker': 'off',
+        'unused-css-rules': 'off',
+        'unused-javascript': 'off',
+        'uses-rel-preconnect': 'off',
+      },
+    },
+  },
+};
