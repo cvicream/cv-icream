1ec0f77cc60674b3e3a2f343204d21a1c59996a5
Ryan Hsieh <chengyu0827@gmail.com>
2023-05-20 15:47:14 +0100

    update lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,15 +2,15 @@ module.exports = {
   ci: {
     assert: {
       preset: 'lighthouse:recommended',
-    },
-    assertions: {
-      'color-contrast': 'off',
-      'csp-xss': 'off',
-      'font-display': 'off',
-      'meta-viewport': 'off',
-      'service-worker': 'off',
-      'unused-css-rules': 'off',
-      'unused-javascript': 'off',
+      assertions: {
+        'color-contrast': 'off',
+        'csp-xss': 'off',
+        'font-display': 'off',
+        'meta-viewport': 'off',
+        'service-worker': 'off',
+        'unused-css-rules': 'off',
+        'unused-javascript': 'off',
+      },
     },
   },
 };
