ecfb36524c6de5c3c7cded51936667bad2c884a7
Ryan Hsieh <chengyu0827@gmail.com>
2023-05-20 15:39:53 +0100

    update assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,5 +3,14 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
     },
+    assertions: {
+      'color-contrast': 'off',
+      'csp-xss': 'off',
+      'font-display': 'off',
+      'meta-viewport': 'off',
+      'service-worker': 'off',
+      'unused-css-rules': 'off',
+      'unused-javascript': 'off',
+    },
   },
 };
