5632a794b40aca7b56ef7edc22947b2f4980ffb0
Ryan Hsieh <chengyu0827@gmail.com>
2023-09-09 13:41:19 +0100

    update z-index on link tooltip

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -385,7 +385,7 @@ export default defineComponent({
     <div
       v-if="toolbarVisible && linkTooltipVisible && !linkEditVisible"
       ref="linkTooltip"
-      class="absolute z-10 flex justify-between px-3 py-2 bg-white border-1 border-black rounded-xl shadow-custom"
+      class="absolute z-20 flex justify-between px-3 py-2 bg-white border-1 border-black rounded-xl shadow-custom"
       :style="linkTooltipStyle"
     >
       <a
@@ -414,7 +414,7 @@ export default defineComponent({
     <div
       v-if="linkEditVisible"
       ref="linkEdit"
-      class="absolute z-10 bg-white p-4 rounded-[1.25rem] shadow-custom"
+      class="absolute z-20 bg-white p-4 rounded-[1.25rem] shadow-custom"
       :style="linkEditStyle"
     >
       <div class="flex justify-between">
