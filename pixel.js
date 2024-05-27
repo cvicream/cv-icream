0e24e228e0f49f74f53b2b4e7063f1542f7916ef
Zack Chu <zack.chu@validusrm.com>
2024-05-27 11:16:46 +0100

    test

--- pixel.js
+++ pixel.js
@@ -7,7 +7,12 @@
   if (!f._fbq)f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'
   n.queue = []; t = b.createElement(e); t.async = !0
   t.src = v; s = b.getElementsByTagName(e)[0]
-  s.parentNode.insertBefore(t, s)
+  try {
+    // s.parentNode.insertBefore(t, s)
+  }
+  catch {
+    console.log('fb error')
+  }
 }(window, document, 'script',
   'https://connect.facebook.net/en_US/fbevents.js'))
 fbq('init', '763048235981810')
