0afcae43a88e34f1e1b3730ac2f3c87fba6ca7db
YUHSIEN CHU <34543459+liuqadnyc@users.noreply.github.com>
2024-10-15 00:05:21 +0100

    Feature/account setting page (#209)
    * finish the page
    
    * test
    
    * test
    
    * add mobile
    
    * add requiredAuth
    
    * fix name
    
    * fix name back
    
    * fix error
    
    ---------
    
    Co-authored-by: Zack Chu <zack.chu@validusrm.com>
    Co-authored-by: Ryan Hsieh <chengyu0827@gmail.com>

---
+++ src/layouts/account.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="h-full">
+    <Header />
+    <div class="flex flex-col items-center overflow-auto custom-scrollbar h-[calc(100%-56px)]">
+      <router-view />
+      <div class="flex flex-col gap-[12px] items-center my-[100px]">
+        <span class="i-origin:logo-cvicream w-[140px] h-[37px]" />
+        <span class="typrgraphy text-blacks-40">
+          Copyright ©2024 CV Icream
+        </span>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/pages/account.vue
@@ -0,0 +1,168 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useAuthStore } from '~/stores/auth'
+
+const router = useRouter()
+const auth = useAuthStore()
+const { user: authUser } = storeToRefs(auth)
+const firstName = ref(authUser.value?.firstName)
+const lastName = ref(authUser.value?.lastName)
+const email = ref(authUser.value?.email)
+const showDeleteModal = ref(false)
+const showLogoutModal = ref(false)
+
+const isGoogle = computed(() => authUser?.value?.email.endsWith('@gmail.com'))
+
+const hasChanged = computed(() => {
+  if (!authUser.value)
+    return false
+  return firstName?.value !== authUser.value.firstName
+  || lastName?.value !== authUser.value.lastName
+  || email?.value !== authUser.value.email
+})
+
+function onSave() {
+  console.log(firstName?.value, 'firstName')
+  console.log(lastName?.value, 'lastName')
+  console.log(email?.value, 'email')
+}
+
+function onDelete() {
+  console.log(authUser.value?.id, 'id')
+  showDeleteModal.value = false
+}
+
+function openDeleteModal() {
+  showDeleteModal.value = true
+}
+
+function closeDeleteModal() {
+  showDeleteModal.value = false
+}
+
+function openLogoutModal() {
+  showLogoutModal.value = true
+}
+
+function closeLogoutModal() {
+  showLogoutModal.value = false
+}
+
+function logout() {
+  auth.logout()
+  router.push('/sign-in')
+}
+
+</script>
+
+<template>
+  <div class="w-[358px] h-[851px] md:w-[700px] md:h-[730px] mt-[32px] bg-primary-5 rounded-[24px] p-[32px] gap-[32px] flex flex-col relative">
+    <button class="w-6 h-6 float-right absolute right-[26px] top-[26px]" @click="router.push('/dashboard')">
+      <span
+        class="i-custom:cancel icon-24"
+      />
+    </button>
+    <div class="leading">
+      Your Account
+    </div>
+    <img width="100" height="100" class="rounded-full" :src="authUser?.avatar ?? ''">
+    <div class="flex gap-[24px]">
+      <Input v-model="firstName" label="First Name" />
+      <Input v-model="lastName" label="Last Name" />
+    </div>
+    <div>
+      <Input v-model="email" label="Email" :disabled="true" />
+      <div v-if="isGoogle" class="note text-blacks-40 mt-[4px]">
+        You're currently using your Google account to log in.
+      </div>
+    </div>
+    <div class="flex justify-end gap-[20px] mt-[32px]">
+      <button
+        class="btn-secondary w-[137px]"
+        @click="router.push('/dashboard')"
+      >
+        <span class="subleading">Cancel</span>
+      </button>
+      <button
+        :disabled="!hasChanged"
+        class="btn-primary w-[137px]"
+        :class="!hasChanged && 'text-blacks-40 bg-blacks-10'"
+        @click="() => onSave()"
+      >
+        <span class="subleading">Save</span>
+      </button>
+    </div>
+    <div class="width-[100%] border border-blacks-10 " />
+    <div class="flex flex-col gap-[24px]">
+      <div>
+        <a class="text-primary-100 border-b border-primary-100 cursor-pointer" @click="openLogoutModal">
+          Log out Account
+        </a>
+        <div class="note text-blacks-60">
+          Temporarily suspend your account. You can access it again anytime by signing back in.
+        </div>
+      </div>
+      <div>
+        <a class="text-primary-100 border-b border-primary-100 cursor-pointer" @click="openDeleteModal">
+          Delete Account
+        </a>
+        <div class="note text-blacks-60">
+          Permanently delete your account and all of your content.
+        </div>
+      </div>
+    </div>
+  </div>
+  <Modal
+    v-show="showDeleteModal"
+    title="Are You Sure?"
+    subtitle="All information saved in this account will be erased forever and cannot be undone."
+    subtitle-style="margin-top: 4;"
+    @close="closeDeleteModal"
+  >
+    <div class="flex flex-col gap-6 mt-8 sm:flex-row sm:justify-between">
+      <button
+        class="btn-secondary px-12 flex-shrink-0"
+        @click="closeDeleteModal"
+      >
+        <span class="subleading">Cancel</span>
+      </button>
+      <button
+        class="btn-primary px-6 flex-grow flex-shrink-0 text-white bg-primary-100"
+        @click="onDelete"
+      >
+        <span class="subleading">Delete Account</span>
+      </button>
+    </div>
+  </Modal>
+  <Modal
+    v-show="showLogoutModal"
+    title="You’re about to log out."
+    subtitle="Your account will be temporarily suspended and can be reactivated anytime by signing in."
+    subtitle-style="margin-top: 4;"
+    @close="closeLogoutModal"
+  >
+    <div class="flex flex-col gap-6 mt-8 sm:flex-row sm:justify-between">
+      <button
+        class="btn-secondary px-12 flex-shrink-0"
+        @click="closeLogoutModal"
+      >
+        <span class="subleading">Cancel</span>
+      </button>
+      <button
+        class="btn-primary px-6 flex-grow flex-shrink-0 text-white bg-primary-100"
+        @click="logout"
+      >
+        <span class="subleading">Log out</span>
+      </button>
+    </div>
+  </Modal>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<route lang="yaml">
+meta:
+  layout: account
+  requiresAuth: true
+</route>
